22233fa8cbb019b8b9a18f287329c4cf098a83ab
Baptiste Veyssiere <baptiste.veyssiere@epitech.eu>
2017-06-07 22:13:49 +0200

    test

---
+++ client_src/message_response.c
@@ -0,0 +1,17 @@
+/*
+** message_response.c for Project-Master in /home/veyssi_b/rendu/tek2/PSU/PSU_2016_myirc
+**
+** Made by Baptiste Veyssiere
+** Login   <veyssi_b@epitech.net>
+**
+** Started on  Wed Jun  7 22:12:03 2017 Baptiste Veyssiere
+** Last update Wed Jun  7 22:13:38 2017 Baptiste Veyssiere
+*/
+
+#include "client.h"
+
+int	message_response(const char *command, t_client *client)
+{
+
+  return (0);
+}
